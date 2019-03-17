e3d6a28beb8b286ebbfe6b3963997168ba9613aa
LeeYuMin <lym91@hatiolab.com>
2019-03-18 00:50:26 +0900

    init

---
+++ src/components/List.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="panel panel-default">
+    <ul>
+      <li>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"> 
+            <div class="box-part text-center">
+              <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
+                          
+              <div class="title">
+                <h4>Instagram</h4>
+              </div>
+                          
+              <div class="text">
+                <span>Lorem ipsum dolor sit amet, id quo eruditi eloquentiam. Assum decore te sed. Elitr scripta ocurreret qui ad.</span>
+              </div>
+                          
+              <a href="#">Learn More</a>
+                          
+              </div>
+          </div>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {
+      msg: 'Basic panel example'
+    }
+  }
+}
+</script>
+<style scoped>
+ul {
+  list-style:none;
+}
+span{
+    font-size:15px;
+}
+a{
+  text-decoration:none; 
+  color: #0062cc;
+  border-bottom:2px solid #0062cc;
+}
+.box{
+    padding:60px 0px;
+}
+
+.box-part{
+    background:#FFF;
+    border-radius:0;
+    padding:60px 10px;
+    margin:30px 0px;
+}
+.text{
+    margin:20px 0px;
+}
+
+.fa{
+     color:#4183D7;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ subMsg }}</h2>
+    <a href="#/list"><img src="../assets/main.jpg"></a>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: '치킨 구인/구직 사이트에 오신 것을 환영합니다.',
+      subMsg: '(아래 이미지를 클릭해주세요)'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+h2 {
+  color: #999;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
